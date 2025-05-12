d858ccee8449255fb35494e602154d21b53b808e
jzolee <106175689+jzolee@users.noreply.github.com>
2025-05-12 19:47:06 +0200

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+        maven("https://jitpack.io") // Ezt kell hozzáadni
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io") // Ezt is
+    }
+}
+
+rootProject.name = "Vibration Monitor"
+include(":app")
